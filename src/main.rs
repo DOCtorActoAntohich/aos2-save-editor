d2f571528ad488977e5b2ab6d110f1decde0fb76
DOCtorActoAntohich <fairy666death@mail.ru>
2024-10-02 22:34:25 +0300

    disable for now

--- src/main.rs
+++ src/main.rs
@@ -13,16 +13,16 @@ fn main() -> anyhow::Result<()> {
 
     let mut file = PlayerFile::from_file(&settings.player_file_path)?;
 
-    file.avatar_character = AvatarCharacter::Empty;
-    file.avatar_background = AvatarBackground::LightGrayBackgroundWithSilhouette;
-    file.title_character_in_background = TitleCharacter::OjHimeWinter;
-    file.title_color = TitleColor::Blue;
-    file.title_text_id = TitleText::LookingForFriends;
-    file.nickname = SizedBinarySection {
-        bytes: b"DOC".into(),
-    };
+    // file.avatar_character = AvatarCharacter::Empty;
+    // file.avatar_background = AvatarBackground::LightGrayBackgroundWithSilhouette;
+    // file.title_character_in_background = TitleCharacter::OjHimeWinter;
+    // file.title_color = TitleColor::Blue;
+    // file.title_text_id = TitleText::LookingForFriends;
+    // file.nickname = SizedBinarySection {
+    //     bytes: b"DOC".into(),
+    // };
 
-    file.save(&settings.player_file_path)?;
+    // file.save(&settings.player_file_path)?;
 
     println!("Saved file");
 
