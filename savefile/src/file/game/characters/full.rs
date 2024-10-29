26838da7eaadacb738d356e46b567450071e14ee
DOCtorActoAntohich <fairy666death@mail.ru>
2024-10-29 19:20:34 +0300

    i knew there was a less boilerplatey way to do it

--- savefile/src/file/game/characters/full.rs
+++ savefile/src/file/game/characters/full.rs
@@ -1,5 +1,3 @@
-use binrw::{BinRead, BinWrite};
-
 use crate::bin_bool::BinBool;
 
 /// Full list of characters.
@@ -7,44 +5,69 @@ use crate::bin_bool::BinBool;
 /// ORDER MATTERS. That's how they are coded in game.
 ///
 /// Also see [`StoryCharacterSheet`].
-#[derive(Debug, Clone, PartialEq, Eq, Hash)]
-pub struct FullCharacterSheet {
-    pub sora: bool,
-    pub alte: bool,
-    pub tsih: bool,
-    pub mira: bool,
-    pub sham: bool,
-    pub nath: bool,
-    pub star_breaker: bool,
-    pub suguri: bool,
-    pub saki: bool,
-    pub iru: bool,
-    pub nanako: bool,
-    pub kae: bool,
-    pub kyoko: bool,
-    pub hime: bool,
-    pub sumika: bool,
-}
-
 #[binrw::binrw]
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
 #[brw(little)]
-pub struct BinarySheet {
-    sora: BinBool,
-    alte: BinBool,
-    tsih: BinBool,
-    mira: BinBool,
-    sham: BinBool,
-    nath: BinBool,
-    star_breaker: BinBool,
-    suguri: BinBool,
-    saki: BinBool,
-    iru: BinBool,
-    nanako: BinBool,
-    kae: BinBool,
-    kyoko: BinBool,
-    hime: BinBool,
-    sumika: BinBool,
+pub struct FullCharacterSheet {
+    #[br(map = From::<BinBool>::from)]
+    #[bw(map = BinBool::from)]
+    pub sora: bool,
+
+    #[br(map = From::<BinBool>::from)]
+    #[bw(map = BinBool::from)]
+    pub alte: bool,
+
+    #[br(map = From::<BinBool>::from)]
+    #[bw(map = BinBool::from)]
+    pub tsih: bool,
+
+    #[br(map = From::<BinBool>::from)]
+    #[bw(map = BinBool::from)]
+    pub mira: bool,
+
+    #[br(map = From::<BinBool>::from)]
+    #[bw(map = BinBool::from)]
+    pub sham: bool,
+
+    #[br(map = From::<BinBool>::from)]
+    #[bw(map = BinBool::from)]
+    pub nath: bool,
+
+    #[br(map = From::<BinBool>::from)]
+    #[bw(map = BinBool::from)]
+    pub star_breaker: bool,
+
+    #[br(map = From::<BinBool>::from)]
+    #[bw(map = BinBool::from)]
+    pub suguri: bool,
+
+    #[br(map = From::<BinBool>::from)]
+    #[bw(map = BinBool::from)]
+    pub saki: bool,
+
+    #[br(map = From::<BinBool>::from)]
+    #[bw(map = BinBool::from)]
+    pub iru: bool,
+
+    #[br(map = From::<BinBool>::from)]
+    #[bw(map = BinBool::from)]
+    pub nanako: bool,
+
+    #[br(map = From::<BinBool>::from)]
+    #[bw(map = BinBool::from)]
+    pub kae: bool,
+
+    #[br(map = From::<BinBool>::from)]
+    #[bw(map = BinBool::from)]
+    pub kyoko: bool,
+
+    #[br(map = From::<BinBool>::from)]
+    #[bw(map = BinBool::from)]
+    pub hime: bool,
+
+    #[br(map = From::<BinBool>::from)]
+    #[bw(map = BinBool::from)]
+    pub sumika: bool,
 }
 
 impl FullCharacterSheet {
@@ -65,110 +88,4 @@ impl FullCharacterSheet {
         hime: true,
         sumika: true,
     };
-}
-
-impl BinRead for FullCharacterSheet {
-    type Args<'a> = <BinarySheet as BinRead>::Args<'a>;
-
-    fn read_options<R: std::io::Read + std::io::Seek>(
-        reader: &mut R,
-        endian: binrw::Endian,
-        args: Self::Args<'_>,
-    ) -> binrw::BinResult<Self> {
-        <BinarySheet as BinRead>::read_options(reader, endian, args).map(Into::into)
-    }
-}
-
-impl BinWrite for FullCharacterSheet {
-    type Args<'a> = <BinarySheet as BinWrite>::Args<'a>;
-
-    fn write_options<W: std::io::Write + std::io::Seek>(
-        &self,
-        writer: &mut W,
-        endian: binrw::Endian,
-        args: Self::Args<'_>,
-    ) -> binrw::BinResult<()> {
-        let binary = self.clone().into();
-        <BinarySheet as BinWrite>::write_options(&binary, writer, endian, args)
-    }
-}
-
-impl From<FullCharacterSheet> for BinarySheet {
-    fn from(
-        FullCharacterSheet {
-            sora,
-            alte,
-            tsih,
-            mira,
-            sham,
-            nath,
-            star_breaker,
-            suguri,
-            saki,
-            iru,
-            nanako,
-            kae,
-            kyoko,
-            hime,
-            sumika,
-        }: FullCharacterSheet,
-    ) -> Self {
-        Self {
-            sora: sora.into(),
-            alte: alte.into(),
-            tsih: tsih.into(),
-            mira: mira.into(),
-            sham: sham.into(),
-            nath: nath.into(),
-            star_breaker: star_breaker.into(),
-            suguri: suguri.into(),
-            saki: saki.into(),
-            iru: iru.into(),
-            nanako: nanako.into(),
-            kae: kae.into(),
-            kyoko: kyoko.into(),
-            hime: hime.into(),
-            sumika: sumika.into(),
-        }
-    }
-}
-
-impl From<BinarySheet> for FullCharacterSheet {
-    fn from(
-        BinarySheet {
-            sora,
-            alte,
-            tsih,
-            mira,
-            sham,
-            nath,
-            star_breaker,
-            suguri,
-            saki,
-            iru,
-            nanako,
-            kae,
-            kyoko,
-            hime,
-            sumika,
-        }: BinarySheet,
-    ) -> Self {
-        Self {
-            sora: sora.into(),
-            alte: alte.into(),
-            tsih: tsih.into(),
-            mira: mira.into(),
-            sham: sham.into(),
-            nath: nath.into(),
-            star_breaker: star_breaker.into(),
-            suguri: suguri.into(),
-            saki: saki.into(),
-            iru: iru.into(),
-            nanako: nanako.into(),
-            kae: kae.into(),
-            kyoko: kyoko.into(),
-            hime: hime.into(),
-            sumika: sumika.into(),
-        }
-    }
 }
--- savefile/src/file/game/characters/story.rs
+++ savefile/src/file/game/characters/story.rs
@@ -1,46 +1,67 @@
-use binrw::{BinRead, BinWrite};
-
 use crate::bin_bool::BinBool;
 
 /// Same as [`FullCharacterSheet`] but doesn't have Sumika.
 ///
 /// Because that silly girl doesn't have her story mode playthrough.
-#[derive(Debug, Clone, PartialEq, Eq, Hash)]
-pub struct StoryCharacterSheet {
-    pub sora: bool,
-    pub alte: bool,
-    pub tsih: bool,
-    pub mira: bool,
-    pub sham: bool,
-    pub nath: bool,
-    pub star_breaker: bool,
-    pub suguri: bool,
-    pub saki: bool,
-    pub iru: bool,
-    pub nanako: bool,
-    pub kae: bool,
-    pub kyoko: bool,
-    pub hime: bool,
-}
-
 #[binrw::binrw]
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
 #[brw(little)]
-pub struct BinarySheet {
-    sora: BinBool,
-    alte: BinBool,
-    tsih: BinBool,
-    mira: BinBool,
-    sham: BinBool,
-    nath: BinBool,
-    star_breaker: BinBool,
-    suguri: BinBool,
-    saki: BinBool,
-    iru: BinBool,
-    nanako: BinBool,
-    kae: BinBool,
-    kyoko: BinBool,
-    hime: BinBool,
+pub struct StoryCharacterSheet {
+    #[br(map = From::<BinBool>::from)]
+    #[bw(map = BinBool::from)]
+    pub sora: bool,
+
+    #[br(map = From::<BinBool>::from)]
+    #[bw(map = BinBool::from)]
+    pub alte: bool,
+
+    #[br(map = From::<BinBool>::from)]
+    #[bw(map = BinBool::from)]
+    pub tsih: bool,
+
+    #[br(map = From::<BinBool>::from)]
+    #[bw(map = BinBool::from)]
+    pub mira: bool,
+
+    #[br(map = From::<BinBool>::from)]
+    #[bw(map = BinBool::from)]
+    pub sham: bool,
+
+    #[br(map = From::<BinBool>::from)]
+    #[bw(map = BinBool::from)]
+    pub nath: bool,
+
+    #[br(map = From::<BinBool>::from)]
+    #[bw(map = BinBool::from)]
+    pub star_breaker: bool,
+
+    #[br(map = From::<BinBool>::from)]
+    #[bw(map = BinBool::from)]
+    pub suguri: bool,
+
+    #[br(map = From::<BinBool>::from)]
+    #[bw(map = BinBool::from)]
+    pub saki: bool,
+
+    #[br(map = From::<BinBool>::from)]
+    #[bw(map = BinBool::from)]
+    pub iru: bool,
+
+    #[br(map = From::<BinBool>::from)]
+    #[bw(map = BinBool::from)]
+    pub nanako: bool,
+
+    #[br(map = From::<BinBool>::from)]
+    #[bw(map = BinBool::from)]
+    pub kae: bool,
+
+    #[br(map = From::<BinBool>::from)]
+    #[bw(map = BinBool::from)]
+    pub kyoko: bool,
+
+    #[br(map = From::<BinBool>::from)]
+    #[bw(map = BinBool::from)]
+    pub hime: bool,
 }
 
 impl StoryCharacterSheet {
@@ -60,106 +81,4 @@ impl StoryCharacterSheet {
         kyoko: true,
         hime: true,
     };
-}
-
-impl BinRead for StoryCharacterSheet {
-    type Args<'a> = <BinarySheet as BinRead>::Args<'a>;
-
-    fn read_options<R: std::io::Read + std::io::Seek>(
-        reader: &mut R,
-        endian: binrw::Endian,
-        args: Self::Args<'_>,
-    ) -> binrw::BinResult<Self> {
-        <BinarySheet as BinRead>::read_options(reader, endian, args).map(Into::into)
-    }
-}
-
-impl BinWrite for StoryCharacterSheet {
-    type Args<'a> = <BinarySheet as BinWrite>::Args<'a>;
-
-    fn write_options<W: std::io::Write + std::io::Seek>(
-        &self,
-        writer: &mut W,
-        endian: binrw::Endian,
-        args: Self::Args<'_>,
-    ) -> binrw::BinResult<()> {
-        let binary: BinarySheet = self.clone().into();
-        <BinarySheet as BinWrite>::write_options(&binary, writer, endian, args)
-    }
-}
-
-impl From<BinarySheet> for StoryCharacterSheet {
-    fn from(
-        BinarySheet {
-            sora,
-            alte,
-            tsih,
-            mira,
-            sham,
-            nath,
-            star_breaker,
-            suguri,
-            saki,
-            iru,
-            nanako,
-            kae,
-            kyoko,
-            hime,
-        }: BinarySheet,
-    ) -> Self {
-        Self {
-            sora: sora.into(),
-            alte: alte.into(),
-            tsih: tsih.into(),
-            mira: mira.into(),
-            sham: sham.into(),
-            nath: nath.into(),
-            star_breaker: star_breaker.into(),
-            suguri: suguri.into(),
-            saki: saki.into(),
-            iru: iru.into(),
-            nanako: nanako.into(),
-            kae: kae.into(),
-            kyoko: kyoko.into(),
-            hime: hime.into(),
-        }
-    }
-}
-
-impl From<StoryCharacterSheet> for BinarySheet {
-    fn from(
-        StoryCharacterSheet {
-            sora,
-            alte,
-            tsih,
-            mira,
-            sham,
-            nath,
-            star_breaker,
-            suguri,
-            saki,
-            iru,
-            nanako,
-            kae,
-            kyoko,
-            hime,
-        }: StoryCharacterSheet,
-    ) -> Self {
-        Self {
-            sora: sora.into(),
-            alte: alte.into(),
-            tsih: tsih.into(),
-            mira: mira.into(),
-            sham: sham.into(),
-            nath: nath.into(),
-            star_breaker: star_breaker.into(),
-            suguri: suguri.into(),
-            saki: saki.into(),
-            iru: iru.into(),
-            nanako: nanako.into(),
-            kae: kae.into(),
-            kyoko: kyoko.into(),
-            hime: hime.into(),
-        }
-    }
 }
