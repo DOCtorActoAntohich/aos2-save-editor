8aa0024563206822ece28bdb5df705ff6edd1821
DOCtorActoAntohich <fairy666death@mail.ru>
2024-10-29 19:14:52 +0300

    test a cool binrw feature with map on r/w

--- savefile/src/bin_bool.rs
+++ savefile/src/bin_bool.rs
@@ -25,6 +25,12 @@ impl From<bool> for BinBool {
     }
 }
 
+impl From<&bool> for BinBool {
+    fn from(&value: &bool) -> Self {
+        Self::from(value)
+    }
+}
+
 impl From<BinBool> for bool {
     fn from(value: BinBool) -> Self {
         match value {
@@ -42,9 +48,7 @@ mod tests {
 
     use super::BinBool;
 
-    #[rstest::rstest]
-    #[case::false_value(b"\x00", false.into())]
-    #[case::true_value(b"\x01", true.into())]
+    #[rstest_reuse::apply(raw_bin_bool)]
     fn encodes_properly(#[case] expected_buffer_content: &[u8], #[case] value: BinBool) {
         let mut writer = Cursor::new(Vec::new());
         value.write(&mut writer).expect("Must write here");
@@ -53,13 +57,49 @@ mod tests {
         assert_eq!(expected_buffer_content, actual_buffer_content);
     }
 
-    #[rstest::rstest]
-    #[case::false_value(b"\x00", false)]
-    #[case::true_value(b"\x01", true)]
+    #[rstest_reuse::apply(raw_bin_bool)]
     fn decodes_properly(#[case] input: &[u8], #[case] expected_value: bool) {
         let mut reader = Cursor::new(input.to_vec());
         let actual_value = BinBool::read(&mut reader).expect("Must read here");
 
         assert_eq!(expected_value, actual_value.into());
     }
+
+    #[binrw::binrw]
+    #[derive(Debug, Clone, PartialEq, Eq)]
+    #[brw(little)]
+    struct Status {
+        #[br(map = From::<BinBool>::from)]
+        #[bw(map = BinBool::from)]
+        is_enabled: bool,
+    }
+
+    #[rstest_reuse::apply(struct_with_bool_field)]
+    fn struct_with_bool_field_encodes(#[case] expected_raw: u8, #[case] input: Status) {
+        let mut writer = binrw::io::Cursor::new([123]);
+        BinWrite::write(&input, &mut writer).expect("Struct must encode here");
+        let [actual_raw] = writer.into_inner();
+
+        assert_eq!(expected_raw, actual_raw);
+    }
+
+    #[rstest_reuse::apply(struct_with_bool_field)]
+    fn struct_with_bool_field_decodes(#[case] input: u8, #[case] expected_struct: Status) {
+        let mut reader = binrw::io::Cursor::new([input]);
+        let actual_struct: Status = BinRead::read(&mut reader).expect("Struct must decode here");
+
+        assert_eq!(expected_struct, actual_struct);
+    }
+
+    #[rstest_reuse::template]
+    #[rstest::rstest]
+    #[case::false_value(b"\x00", false.into())]
+    #[case::true_value(b"\x01", true.into())]
+    fn raw_bin_bool(#[case] expected_buffer_content: &[u8], #[case] value: BinBool) {}
+
+    #[rstest_reuse::template]
+    #[rstest::rstest]
+    #[case(0x00, Status { is_enabled: false })]
+    #[case(0x01, Status { is_enabled: true })]
+    fn struct_with_bool_field(#[case] raw: u8, #[case] status: Status) {}
 }
