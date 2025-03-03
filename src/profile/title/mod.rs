7487a692df7ed7e60c13c0063e52523cb4bea76b
DOCtorActoAntohich <fairy666death@mail.ru>
2025-03-03 21:21:28 +0300

    info text? but why......

--- src/profile/title/mod.rs
+++ src/profile/title/mod.rs
@@ -1,16 +1,30 @@
 use online_profile::PlayerOnlineProfile;
-use ratatui::{buffer::Buffer, crossterm::event::Event, layout::Rect};
+use ratatui::{
+    buffer::Buffer,
+    crossterm::event::Event,
+    layout::{Constraint, Rect},
+    text::Line,
+    widgets::{List, Widget},
+};
 use tokio::sync::watch;
 
 use crate::{
     info::content_window::InteratibleTabComponent,
     tui::{HandleEvent, VisualComponent},
+    widget::split,
 };
 
 use super::table::{color, InteractibleTable, TablesCollection};
 
 pub struct Tab {
     tables: TablesCollection<1>,
+}
+
+struct InfoText;
+
+impl InfoText {
+    const N_LINES: u16 = 3;
+    const CONSTRAINT: Constraint = Constraint::Length(Self::N_LINES);
 }
 
 impl Tab {
@@ -30,12 +44,31 @@ impl HandleEvent for Tab {
 
 impl VisualComponent for Tab {
     fn render(&self, area: Rect, buf: &mut Buffer) {
-        self.tables.render(area, buf);
+        let top = split::Area {
+            constraint: InfoText::CONSTRAINT,
+            render: |area: Rect, buf: &mut Buffer| {
+                InfoText.render(area, buf);
+            },
+        };
+        let bottom = split::Area {
+            constraint: Constraint::Fill(1),
+            render: |area: Rect, buf: &mut Buffer| {
+                self.tables.render(area, buf);
+            },
+        };
+        split::Horizontal { top, bottom }.render(area, buf);
     }
 }
 
 impl InteratibleTabComponent for Tab {
     fn name(&self) -> &'static str {
         "Online Title"
+    }
+}
+
+impl VisualComponent for InfoText {
+    fn render(&self, area: Rect, buf: &mut Buffer) {
+        let lines: [Line<'_>; Self::N_LINES as usize] = ["a".into(), "b".into(), "c".into()];
+        List::new(lines).render(area, buf);
     }
 }
