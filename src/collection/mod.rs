fd61467ffa2fc1c84427649928b0ef55fb11b059
DOCtorActoAntohich <fairy666death@mail.ru>
2025-03-15 16:08:58 +0300

    remove unused collection

--- src/collection/mod.rs
+++ src/collection/mod.rs
@@ -1,11 +1,9 @@
 mod hovering_index;
-mod radio_button_array;
 mod selectable_array;
 mod slice;
 mod text_search;
 
 pub use self::hovering_index::{HoveringIndex, RadioButtonIndex};
-pub use self::radio_button_array::RadioButtonArray;
 pub use self::selectable_array::SelectableArray;
 pub use self::slice::ListSlice;
 pub use self::text_search::TextSearch;
