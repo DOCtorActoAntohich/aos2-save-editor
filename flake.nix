e3673367d8abd4a0d809747e38b84bf4bce00770
DOCtorActoAntohich <fairy666death@mail.ru>
2025-07-26 15:01:47 +0300

    better flake

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     flake-utils.url = "github:numtide/flake-utils";
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
@@ -8,23 +8,47 @@
     };
   };
 
-  outputs = { self, nixpkgs, rust-overlay, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      overlays = [ (import rust-overlay) ];
-      pkgs = import nixpkgs { inherit system overlays; };
-      rust = pkgs.rust-bin.stable."1.86.0".minimal.override {
-        extensions = [
-	  "rustfmt"
-	  "clippy"
-	  "rust-analyzer"
-	  "rust-src"
-	];
-      };
-    in {
-      devShells.default = pkgs.mkShell {
-        packages = [
-	  rust
-	];
-      };
-    });
+  outputs =
+    {
+      nixpkgs,
+      rust-overlay,
+      flake-utils,
+      ...
+    }:
+    let
+      overlays = [
+        (import rust-overlay)
+      ];
+      supported-systems = [
+        "x86_64-linux"
+      ];
+      mk-rust-platform = { pkgs, ... }: pkgs.rust-bin.stable."1.86.0".default;
+      mk-dev-shell = { pkgs, rust-platform, ... }: pkgs.mkShell { packages = [ rust-platform ]; };
+      mk-build =
+        {
+          nixpkgs,
+          system,
+          overlays,
+          ...
+        }:
+        let
+          pkgs = import nixpkgs { inherit system overlays; };
+          rust-platform = mk-rust-platform { inherit pkgs; };
+          dev-shell = mk-dev-shell { inherit pkgs rust-platform; };
+        in
+        {
+          inherit dev-shell;
+        };
+    in
+    {
+      devShells = nixpkgs.lib.genAttrs supported-systems (
+        system:
+        let
+          build = mk-build { inherit nixpkgs system overlays; };
+        in
+        {
+          default = build.dev-shell;
+        }
+      );
+    };
 }
