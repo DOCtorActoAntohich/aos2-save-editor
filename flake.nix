979526fe202caa1d5b489b9feaf5d42330efc528
DOCtorActoAntohich <fairy666death@mail.ru>
2025-08-07 22:28:54 +0300

    build the app

--- flake.nix
+++ flake.nix
@@ -6,14 +6,16 @@
       url = "github:oxalica/rust-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    nix-filter.url = "github:numtide/nix-filter";
   };
 
   outputs =
     {
+      self,
       nixpkgs,
       rust-overlay,
       flake-utils,
-      ...
+      nix-filter,
     }:
     let
       overlays = [
@@ -22,8 +24,61 @@
       supported-systems = [
         "x86_64-linux"
       ];
-      mk-rust-platform = { pkgs, ... }: pkgs.rust-bin.stable."1.86.0".default;
-      mk-dev-shell = { pkgs, rust-platform, ... }: pkgs.mkShell { packages = [ rust-platform ]; };
+
+      mk-rust =
+        {
+          pkgs,
+        }:
+        pkgs.rust-bin.stable."1.86.0".default;
+
+      mk-rust-platform =
+        { pkgs }:
+        let
+          rust = mk-rust { inherit pkgs; };
+        in
+        pkgs.makeRustPlatform {
+          rustc = rust;
+          cargo = rust;
+        };
+
+      read-package-version =
+        { cargo-toml }:
+        let
+          manifest = builtins.fromTOML (builtins.readFile cargo-toml);
+        in
+        manifest.workspace.package.version;
+
+      mk-editor =
+        { pkgs, rust-platform }:
+        let
+          crate-version = read-package-version { cargo-toml = ./Cargo.toml; };
+        in
+        rust-platform.buildRustPackage {
+          pname = "aos2-save-editor";
+          version = crate-version;
+          src = nix-filter {
+            root = ./.;
+            include = [
+              "crates"
+              "examples"
+              "src"
+              "Cargo.toml"
+              "Cargo.lock"
+              "build.rs"
+            ];
+          };
+          cargoLock = {
+            lockFile = ./Cargo.lock;
+            allowBuiltinFetchGit = true;
+          };
+        };
+
+      mk-dev-shell =
+        { pkgs, editor }:
+        pkgs.mkShell {
+          inputsFrom = [ editor ];
+        };
+
       mk-build =
         {
           nixpkgs,
@@ -34,13 +89,24 @@
         let
           pkgs = import nixpkgs { inherit system overlays; };
           rust-platform = mk-rust-platform { inherit pkgs; };
-          dev-shell = mk-dev-shell { inherit pkgs rust-platform; };
+          editor = mk-editor { inherit pkgs rust-platform; };
+          dev-shell = mk-dev-shell { inherit pkgs editor; };
         in
         {
-          inherit dev-shell;
+          inherit dev-shell editor;
         };
     in
     {
+      packages = nixpkgs.lib.genAttrs supported-systems (
+        system:
+        let
+          build = mk-build { inherit nixpkgs system overlays; };
+        in
+        {
+          default = build.editor;
+          editor = build.editor;
+        }
+      );
       devShells = nixpkgs.lib.genAttrs supported-systems (
         system:
         let
