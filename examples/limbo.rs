fbcbb9f98016c338500dfedbe08c6053c690b758
DOCtorActoAntohich <doctoractoantohich@gmail.com>
2026-02-15 01:09:38 +0300

    i broke the example i fixed the example

--- examples/limbo.rs
+++ examples/limbo.rs
@@ -4,7 +4,12 @@ fn main() -> anyhow::Result<()> {
     let mut terminal = ratatui::init();
     terminal.clear()?;
 
-    let error = aos2_save_editor::savefile::Error::Progress(player_progress::Error::NotFound);
+    let error = aos2_save_editor::savefile::Error::Progress(binary_file::Error {
+        path: aos2_env::saves_location(aos2_env::EXAMPLE_HOME)
+            .join(player_progress::PlayerProgress::FILE_NAME),
+        action: binary_file::ErroneousAction::Reading,
+        detail: binary_file::ErrorDetail::NotFound,
+    });
     let app_result = aos2_save_editor::App::new_limbo(error).run(&mut terminal);
 
     ratatui::restore();
