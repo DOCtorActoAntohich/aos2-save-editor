960765a0b65a39d564cbcd69f75cf6522cf8802d
DOCtorActoAntohich <doctoractoantohich@gmail.com>
2026-02-20 20:35:58 +0300

    allow canonical paths to be anywhere, not just in aos2 saves directory

--- crates/easydiff/src/path.rs
+++ crates/easydiff/src/path.rs
@@ -1,3 +1,4 @@
+use std::io::ErrorKind;
 use std::path::{Path, PathBuf};
 
 use anyhow::Context;
@@ -8,10 +9,28 @@ pub struct CanonicalSaveFilePath(PathBuf);
 
 impl CanonicalSaveFilePath {
     pub fn new(saves: impl AsRef<Path>, file_name: impl AsRef<Path>) -> anyhow::Result<Self> {
+        match file_name.as_ref().canonicalize() {
+            Ok(real) => return Ok(Self(real)),
+            Err(err) if err.kind() == ErrorKind::NotFound => (),
+            Err(err) => {
+                return Err(err).with_context(|| {
+                    anyhow::anyhow!(
+                        "Failed to canonicalize path: {}",
+                        file_name.as_ref().display()
+                    )
+                });
+            }
+        };
+
         let joined = saves.as_ref().join(file_name);
         joined
             .canonicalize()
-            .with_context(|| anyhow::anyhow!("Failed to canonicalize path: {}", joined.display()))
+            .with_context(|| {
+                anyhow::anyhow!(
+                    "Failed to canonicalize path in AoS2 directory: {}",
+                    joined.display()
+                )
+            })
             .map(Self)
     }
 }
